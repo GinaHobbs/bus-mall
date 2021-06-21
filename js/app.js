c50f89c76575456a8544e145c73e22d3cfcc9f0b
GinaHobbs <ginamariehobbs@gmail.com>
2021-06-21 17:25:35 -0500

    votes are being processed- working on rendering results to the screen

--- js/app.js
+++ js/app.js
@@ -2,16 +2,91 @@
 
 Product.allProducts = [];
 let mainElem = document.getElementById('main')
-let h1Elem = document.getElementById('h1');
-let imgElem = document.getElementById('img')
+let h1LeftElem = document.getElementById('left-h1');
+let imgLeftElem = document.getElementById('left-img')
+let h1MiddleElem = document.getElementById('middle-h1');
+let imgMiddleElem = document.getElementById('middle-img')
+let h1RightElem = document.getElementById('right-h1');
+let imgRightElem = document.getElementById('right-img')
+let resultsUlElem = document.getElementById('results');
+let voteCounter = 0;
+let leftProduct = null;
+let middleProduct = null;
+let rightProduct = null;
 
 function Product(name, image) {
 this.name = name;
 this.image = image;
 this.viewedCounter = 0;
+this.votes = 0;
 
 Product.allProducts.push(this);
 }
+
+function pickThreeProducts() {
+let leftProductIndex = Math.floor((Math.random() * Product.allProducts.length));
+leftProduct = Product.allProducts[leftProductIndex];
+
+let middleProductIndex = Math.floor((Math.random() * Product.allProducts.length));
+middleProduct = Product.allProducts[middleProductIndex];
+
+while (middleProduct === null || middleProductIndex === leftProductIndex) {
+  let middleProductIndex = Math.floor((Math.random() * Product.allProducts.length));
+  middleProduct = Product.allProducts[middleProductIndex];
+}
+
+let rightProductIndex = Math.floor((Math.random() * Product.allProducts.length));
+rightProduct = Product.allProducts[rightProductIndex];
+
+while (rightProduct === null || rightProductIndex === middleProductIndex || rightProductIndex === leftProductIndex) {
+  let rightProductIndex = Math.floor((Math.random() * Product.allProducts.length));
+  rightProduct = Product.allProducts[rightProductIndex];
+}
+
+leftProduct.renderProduct(h1LeftElem, imgLeftElem);
+middleProduct.renderProduct(h1MiddleElem, imgMiddleElem);
+rightProduct.renderProduct(h1RightElem, imgRightElem);
+}
+
+Product.prototype.renderProduct = function(h1, img) {
+h1.textContent = this.name;
+img.src = this.image;
+this.viewedCounter++;
+}
+
+function handleClick(event) {
+let id = event.target.id
+if (id === 'left-img' || id === 'middle-img' || id === 'right-img') {
+  voteCounter++;
+  if (id === 'left-img') {
+    leftProduct.votes++;
+  } else if (id === 'middle-img'){
+    middleProduct.votes++;
+  } else {
+    rightProduct.votes++;
+  }
+  pickThreeProducts();
+} else {
+  alert('try again');
+}
+if (voteCounter === 10) {
+  renderResults();
+  // turn off the listener
+  mainElem.removeEventListener('click', handleClick);
+}
+}
+
+function renderResults() {
+  resultsUlElem.innerHTML = "";
+
+  for (let product of Product.allProducts) {
+    let liElem = document.createElement('li');
+    liElem.textContent = `${product.name}: ${product.votes}`;
+    resultsUlElem.appendChild(liElem);
+  }
+}
+
+mainElem.addEventListener('click', handleClick);
 
 new Product('Banana Slicer', '../img/banana.jpeg');
 new Product('Bathroom Ipad Stand', '../img/bathroom.jpeg');
@@ -32,18 +107,4 @@ new Product('Unicorn Meat', '../img/unicorn.jpeg');
 new Product('Perpetual Watering Can', '../img/water-can.jpeg');
 new Product('Wine Glass With Hole', '../img/wine-glass.jpeg');
 
-
-Product.prototype.renderProduct = function(h1, img) {
-h1.textContent = this.name;
-img.src = this.image;
-this.viewedCounter++;
-}
-
-
-
-function handleClick() {
-  Product.allProducts[0].renderProduct(h1Elem, imgElem);
-}
-
-h1Elem.addEventListener('click', handleClick);
-
+pickThreeProducts();
